ea1e0bde7648047e2e31f5a154ffa3287dc1e0ab
shushunchyk <martavolnianska@gmail.com>
2026-04-05 20:00:37 +0200

    Refactor GameObject to use float for position and velocity
    Updated GameObject to use float for position and velocity, added getters and setters for velocity.

--- src/GameObject.java
+++ src/GameObject.java
@@ -2,10 +2,12 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public abstract class GameObject {
-    protected int x, y;
+
+    protected float x, y;
+    protected float velX, velY;
     protected int width, height;
 
-    public GameObject(int x, int y, int width, int height) {
+    public GameObject(float x, float y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -16,6 +18,25 @@ public abstract class GameObject {
     public abstract void render(Graphics g);
 
     public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
+        return new Rectangle((int)x, (int)y, width, height);
     }
+
+    // --- Getters and Setters ---
+    public float getX() { return x; }
+    public void setX(float x) { this.x = x; }
+
+    public float getY() { return y; }
+    public void setY(float y) { this.y = y; }
+
+    public float getVelX() { return velX; }
+    public void setVelX(float velX) { this.velX = velX; }
+
+    public float getVelY() { return velY; }
+    public void setVelY(float velY) { this.velY = velY; }
+
+    public int getWidth() { return width; }
+    public void setWidth(int width) { this.width = width; }
+
+    public int getHeight() { return height; }
+    public void setHeight(int height) { this.height = height; }
 }
